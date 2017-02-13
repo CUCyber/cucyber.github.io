669e29a91c044480967958087c3644acf5689ad0
Foster McLane <fkmclane@gmail.com>
2017-02-13 10:08:54 -0500

    fix small issues with header and style

--- js/main.js
+++ js/main.js
@@ -1,90 +1,52 @@
-(function($){
-    $.fn.scrollingTo = function( opts ) {
-        var defaults = {
-            animationTime : 1000,
-            easing : '',
-            callbackBeforeTransition : function(){},
-            callbackAfterTransition : function(){}
-        };
-
-        var config = $.extend( {}, defaults, opts );
-
-        $(this).click(function(e){
-            var eventVal = e;
-            e.preventDefault();
-
-            var $section = $(document).find( $(this).data('section') );
-            if ( $section.length < 1 ) {
-                return false;
-            };
-
-            if ( $('html, body').is(':animated') ) {
-                $('html, body').stop( true, true );
-            };
-
-            var scrollPos = $section.offset().top;
-
-            if ( $(window).scrollTop() == scrollPos ) {
-                return false;
-            };
-
-            config.callbackBeforeTransition(eventVal, $section);
-
-            $('html, body').animate({
-                'scrollTop' : (scrollPos+'px' )
-            }, config.animationTime, config.easing, function(){
-                config.callbackAfterTransition(eventVal, $section);
-            });
-        });
+$.fn.scrollingTo = function(opts) {
+    var defaults = {
+        animationTime : 1000,
+        easing : '',
+        callbackBeforeTransition : function() {},
+        callbackAfterTransition : function() {}
     };
-}(jQuery));
 
+    var config = $.extend({}, defaults, opts);
 
+    $(this).click(function(e) {
+        e.preventDefault();
 
-jQuery(document).ready(function(){
-	"use strict";
-	new WOW().init();
+        var section = $(document).find($(this).data('section'));
+        if (section.length < 1)
+            return false;
 
+        if ($('html, body').is(':animated'))
+            $('html, body').stop(true, true);
 
-(function(){
- jQuery('.smooth-scroll').scrollingTo();
-}());
+        var scrollPos = section.offset().top;
 
+        if ($(window).scrollTop() == scrollPos)
+            return false;
+
+        config.callbackBeforeTransition(e, section);
+
+        $('html, body').animate({'scrollTop' : (scrollPos + 'px')}, config.animationTime, config.easing, function() {
+            config.callbackAfterTransition(e, section);
+        });
+    });
+};
+
+$(document).ready(function() {
+    new WOW().init();
+
+    jQuery('.smooth-scroll').scrollingTo();
 });
 
-
-
-
 $(document).ready(function(){
-
-
-
-
-    $(window).scroll(function () {
-        if ($(window).scrollTop() > 50) {
-            $(".navbar-brand a").css("color","#fff");
-            $("#top-bar").removeClass("animated-header");
-        } else {
-            $(".navbar-brand a").css("color","inherit");
-            $("#top-bar").addClass("animated-header");
-        }
-    });
-
-    $("#clients-logo").owlCarousel({
- 
+    $('#clients-logo').owlCarousel({
         itemsCustom : false,
         pagination : false,
         items : 5,
         autoplay: true,
-
-    })
-
+    });
 });
 
-
-
-// fancybox
-$(".fancybox").fancybox({
+$('.fancybox').fancybox({
     padding: 0,
 
     openEffect : 'elastic',
@@ -95,8 +57,8 @@ $(".fancybox").fancybox({
 
     closeClick : true,
     helpers : {
-        title : { 
-            type: 'inside' 
+        title : {
+            type: 'inside'
         },
         overlay : {
             css : {
@@ -105,14 +67,3 @@ $(".fancybox").fancybox({
         }
     }
 });
-
-
-
-
-
-
- 
-
-
-
-

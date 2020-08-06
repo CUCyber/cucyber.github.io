a24f8c39142116f41cb928c30dd1edeec6966049
David Houston <houstdav000@gmail.com>
2020-08-06 08:26:59 -0400

    Add blinking block cursor

--- js/txt-rotate.js
+++ js/txt-rotate.js
@@ -69,9 +69,4 @@ window.onload = function() {
       new TxtRotate(elements[i], JSON.parse(toRotate), period);
     }
   }
-  // INJECT CSS
-  var css = document.createElement("style");
-  css.type = "text/css";
-  css.innerHTML = ".txt-rotate > .wrap { border-right: 0.1em solid #666 }";
-  document.body.appendChild(css);
 };
